d14ced768e10f4a64846d46e9c615c824bb8d527
Duncan <bonsairobo@gmail.com>
2021-10-20 19:38:07 -0700

    Remove the README code example.
    It seemed a bit contrived. Maybe we can come up with something better later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,10 @@
 //! Generic math on [integer lattices](https://en.wikipedia.org/wiki/Integer_lattice) (regular grids).
 //!
-//! This crate provides types and traits that make it easy to write generic code with integer lattices, i.e. `Z^N`. In
-//! particular, we provide implementations of [`IntegerVector`](crate::vector::IntegerVector) for `glam`'s
+//! This crate provides types and traits that make it easy to write generic code with integer lattices, i.e. Z<sup>2</sup> and
+//! Z<sup>3</sup>. In particular, we provide implementations of [`IntegerVector`](crate::vector::IntegerVector) for `glam`'s
 //! [`IVec2`](glam::IVec2), [`IVec3`](glam::IVec3), [`UVec2`](glam::UVec2) and [`UVec3`](glam::UVec3) types. There are also some
 //! traits that apply to vectors with real number scalars as well, and those are implemented for [`Vec2`](glam::Vec2),
 //! [`Vec3`](glam::Vec3), and [`Vec3A`](glam::Vec3A).
-//!
-//! # Example Code
-//! ```
-//! use ilattice::prelude::*;
-//!
-//! // A useful type for working with bounded subsets of the lattice.
-//! const EXTENT: Extent<IVec3> = Extent::from_min_and_shape(
-//!     const_ivec3!([-1; 3]),
-//!     const_ivec3!([5; 3])
-//! );
-//!
-//! assert!(EXTENT.contains(IVec3::new(-1, 3, 0)));
-//!
-//! EXTENT.iter3().for_each(|v| { assert!(EXTENT.contains(v)); });
-//!
-//! // Some bitwise logic and shifting code that works for any integer vector.
-//! fn do_vector_math<V: IntegerVector>(v: V, mask: V, shape_log2: V) -> V {
-//!     (v & mask) >> shape_log2
-//! }
-//!
-//! let v = do_vector_math(IVec2::new(0xA1, 0xB2), IVec2::new(0xF0, 0xF0), IVec2::new(3, 3));
-//! assert_eq!(v, IVec2::new(20, 22));
-//! ```
 
 #![deny(clippy::missing_inline_in_public_items)]
 
